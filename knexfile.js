a551cc8eb090b1e27248d45fe3693f677a64da66
sauravpandey538 <sauravpandey0325@icloud.com>
2025-02-24 11:16:40 +0545

    supabase database

--- knexfile.js
+++ knexfile.js
@@ -26,11 +26,17 @@ const knexConfig = {
   },
   production: {
     client: "pg",
-    connection: process.env.POSTGRES_URL,
-    ssl: {
-      rejectUnauthorized: false,
-      sslmode: "require",
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      port: 5432, // ✅ Default PostgreSQL port
+      user: process.env.POSTGRES_USER, // Change to your PostgreSQL username
+      password: process.env.POSTGRES_PASSWORD, // Change to your PostgreSQL password
+      database: process.env.POSTGRES_DATABASE, // Change to your database name
     },
+    // ssl: {
+    //   rejectUnauthorized: false,
+    //   sslmode: "require",
+    // },
     migrations: {
       tableName: "knex_migrations",
       directory: "./migrations",
